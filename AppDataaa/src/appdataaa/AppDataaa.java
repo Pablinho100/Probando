6f3ef6ef66139f76d29f9e21e12a58f00667bac2
Pablinho100 <pabloalearan@gmail.com>
2023-05-16 20:15:35 -0300

    V3

--- AppDataaa/src/appdataaa/AppDataaa.java
+++ AppDataaa/src/appdataaa/AppDataaa.java
@@ -16,6 +16,9 @@ public class AppDataaa {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("HOLA MUNDO");
+        
     }
     
 }
